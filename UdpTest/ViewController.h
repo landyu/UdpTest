af41a4ced7277fba7966fd1710a6b3a0eb66d10f
Landyu <landyu@LandyusMacBookPro.local>
2015-06-09 14:45:35 +0800

    Initial Commit

---
+++ UdpTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UdpTest
+//
+//  Created by Landyu on 15/6/9.
+//  Copyright (c) 2015年 Landyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UdpTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UdpTest
+//
+//  Created by Landyu on 15/6/9.
+//  Copyright (c) 2015年 Landyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
